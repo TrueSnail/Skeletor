f679e46206cfe5b65a6ec21b57f1e4b61c3fcbe4
Kamil Kwiatkowski <kamilkwiatkos@gmail.com>
2025-12-30 03:21:02 +0100

    Bugged physical model

--- Skeletor_Unity/Assets/Scripts/CharacterHandler.cs
+++ Skeletor_Unity/Assets/Scripts/CharacterHandler.cs
@@ -11,18 +11,28 @@ public class CharacterHandler : MonoBehaviour
     public Transform CharacterOrigin;
     public bool PinToWorldRoot = false;
     public bool DrawDebugPose = true;
+    public float Spring = 10;
+    public float Damper = 1;
 
     [Header("TEST")]
     public Transform Hips;
+    public ConfigurableJoint HipsJ;
+    public Transform ArmatureTest;
     public Transform LeftLegUp;
     public Transform RightLegUp;
+    public ConfigurableJoint RightLegUpJ;
     public Transform Neck;
+    public ConfigurableJoint NeckJ;
     public Transform LeftArm;
+    public ConfigurableJoint LeftArmJ;
     public Transform LeftForearm;
     public Transform RightArm;
+    public ConfigurableJoint RightArmJ;
     public Transform RightForearm;
+    public ConfigurableJoint RightForearmJ;
     public Transform LeftKnee;
     public Transform RightKnee;
+    public ConfigurableJoint RightKneeJ;
 
     private void Start()
     {
@@ -40,6 +50,11 @@ public class CharacterHandler : MonoBehaviour
         UpdateCharacter();
     }
 
+    private void Update()
+    {
+        UpdateJointDrives();
+    }
+
     public void UpdateCharacter()
     {
         var pose = UnityRotationPose.FromPose(PoseData);
@@ -47,17 +62,43 @@ public class CharacterHandler : MonoBehaviour
 
         Hips.position = PinToWorldRoot ? CharacterOrigin.position : CharacterOrigin.position + pose.Position;
 
+        var rot = Hips.localRotation;
         Hips.localRotation = LocalRotation(Hips, pose.Rotation);
         LeftLegUp.localRotation = LocalRotation(LeftLegUp, pose.LeftUpLeg);
-        RightLegUp.localRotation = LocalRotation(RightLegUp, pose.RightUpLeg);
+        //RightLegUp.localRotation = LocalRotation(RightLegUp, pose.RightUpLeg);
+        RightLegUpJ.targetRotation = LocalRotation(RightLegUp, pose.RightUpLeg);
         LeftKnee.localRotation = LocalRotation(LeftKnee, pose.LeftLeg);
-        RightKnee.localRotation = LocalRotation(RightKnee, pose.RightLeg);
+        //RightKnee.localRotation = LocalRotation(RightKnee, pose.RightLeg);
+        RightKneeJ.targetRotation = LocalRotation(RightKnee, pose.RightLeg);
         LeftArm.localRotation = LocalRotation(LeftArm, pose.LeftArm);
-        RightArm.localRotation = LocalRotation(RightArm, pose.RightArm);
+        //LeftArmJ.targetRotation = LocalRotation(LeftArm, pose.LeftArm);
+        //RightArm.localRotation = LocalRotation(RightArm, pose.RightArm);
+        RightArmJ.targetRotation = LocalRotation(RightArm, pose.RightArm);
         LeftForearm.localRotation = LocalRotation(LeftForearm, pose.LeftForeArm);
-        RightForearm.localRotation = LocalRotation(RightForearm, pose.RightForeArm);
-        Neck.localRotation = LocalRotation(Neck, pose.Neck);
-        Hips.localRotation = LocalRotation(Hips, pose.Rotation) * CharacterOrigin.rotation;
+        //RightForearm.localRotation = LocalRotation(RightForearm, pose.RightForeArm);
+        RightForearmJ.targetRotation = LocalRotation(RightForearm, pose.RightForeArm);
+        //Neck.localRotation = LocalRotation(Neck, pose.Neck);
+        NeckJ.targetRotation = LocalRotation(Neck, pose.Neck)/* * Quaternion.Euler(-90, 0, 0)*/;
+        //Hips.localRotation = LocalRotation(Hips, pose.Rotation) * CharacterOrigin.rotation;
+        Hips.localRotation = rot;
+        HipsJ.targetRotation = LocalRotation(Hips, pose.Rotation) * CharacterOrigin.rotation;
+    }
+
+    private void UpdateJointDrives()
+    {
+        UpdateJointDrive(HipsJ);
+        UpdateJointDrive(NeckJ);
+        UpdateJointDrive(RightArmJ);
+        UpdateJointDrive(RightForearmJ);
+        UpdateJointDrive(RightLegUpJ);
+    }
+
+    private void UpdateJointDrive(ConfigurableJoint joint)
+    {
+        var drive = joint.slerpDrive;
+        drive.positionSpring = Spring;
+        drive.positionDamper = Damper;
+        joint.slerpDrive = drive;
     }
 
     private void OnDrawGizmos()
@@ -105,5 +146,14 @@ public class CharacterHandler : MonoBehaviour
 
     }
 
-    private Quaternion LocalRotation(Transform transform, Quaternion worldRotation) => Quaternion.Inverse(transform.parent.rotation) * worldRotation;
+    private Quaternion LocalRotation(Transform transform, Quaternion worldRotation)
+    {
+        var parentRotation = transform.parent.rotation;
+        //if (transform.parent == ArmatureTest) parentRotation = transform.parent.parent.GetComponent<ConfigurableJoint>().targetRotation;
+        //if (transform.parent.TryGetComponent<ConfigurableJoint>(out var joint))
+        //{
+        //    parentRotation = joint.targetRotation;
+        //}
+        return Quaternion.Inverse(parentRotation) * worldRotation;
+    }
 }
